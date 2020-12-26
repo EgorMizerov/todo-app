caf74cfb44bce7129a3f36aad5c11fc2f88e3a24
Egor Mizerov <mizerov-2003@mail.ru>
2020-12-26 20:10:25 +0500

    add config file

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/EgorMizerov/todo-app/pkg/handler"
 	"github.com/EgorMizerov/todo-app/pkg/repository"
 	"github.com/EgorMizerov/todo-app/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handler := handler.NewHandler(services)
 
 	server := new(todo.Server)
-	if err := server.Run("8000", handler.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	return viper.ReadInConfig()
 }
